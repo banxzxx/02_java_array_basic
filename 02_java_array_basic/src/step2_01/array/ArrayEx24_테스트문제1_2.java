6ad52a669d7f52d64730be09c523c60c907711e2
banxzxx <jwbang1643@gmail.com>
2023-03-21 17:25:29 +0900

    0321 공부

--- 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_2.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_2.java
@@ -1,32 +1,89 @@
 package step2_01.array;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+//2023.03.21 12:15 ~ 12:29
+
 public class ArrayEx24_테스트문제1_2 {
 
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner(System.in);
 		
 		// 문제 8) 아래 d에서 scanner로 내가 입력한 값만 빼고 e에 저장 
 		int[] d = { 10,20,30,40,50 };
 		int[] e = { 0,0,0,0,0 };
 		// 예) 30 ==> e = {10,20,40,50,0};
+		
+		
+		System.out.print("값 입력 >> ");
+		int val = scan.nextInt();
+		
+		for (int i = 0, j = 0 ; i < d.length; i++) {
+			if ( d[i] == val ) continue;
+			e[j] = d[i];
+			j++;
+		}
+		System.out.println(Arrays.toString(e));
+		
 
 		// 문제 9) 아래 f에서 scanner로 내가 입력한 번호와 값을 빼고 d에 저장 
 		int[] f = { 1001, 40, 1002, 65, 1003,  70 };
 		int[] g = { 0,0,0,0,0,0 };
 		// 예) 1002 ==> {1001, 40, 1003, 70 , 0, 0};
 		
+		System.out.print("값 입력 >> ");
+		val = scan.nextInt();
+		
+		for (int i = 0, j = 0 ; i < f.length; i++) {
+			if( f[i] == val ) {
+				i++;
+				continue;
+			}
+			g[j] = f[i];
+			j++;
+		}
+		System.out.println(Arrays.toString(g));
+		
 		// 문제 10) 숫자를 하나 입력받고 아래 배열을 앞으로 하나씩 밀어낸후 맨뒤에 저장
 		int[] h = { 10,20,30,40,50 };
 		// 예)  60 ==> {20,30,40,50,60};
 		
+		System.out.print("값 입력 >> ");
+		val = scan.nextInt();
+		
+		for (int i = 0; i < h.length - 1; i++) {
+			h[i] = h[i+1];
+		}
+		h[4] = val;
+		
+		System.out.println(Arrays.toString(h));
+		
 		// 문제 11) 숫자를 하나 입력받고 아래 배열을 뒤로 하나씩 밀어낸후 맨 앞에 저장 
-		int[] i = { 10,20,30,40,50 };
+		int[] z = { 10,20,30,40,50 };
 		// 예) 60 ==> {60,10,20,30,40};
 		
+		System.out.print("값 입력 >> ");
+		val = scan.nextInt();
+		
+		for (int i = z.length - 1; i > 0 ; i--) {
+			z[i] = z[i-1];
+		}
+		z[0] = val;
+		
+		System.out.println(Arrays.toString(z));
+		
 		// 문제 12) 아래배열을 거꾸로 저장 
-		int[] j = { 1,2,3,4,5 };
+		int[] p = { 1,2,3,4,5 };
 		int[] k = { 0,0,0,0,0 };
 		//예) k => {5,4,3,2,1};
+		
+		for (int i = 0, j = 4 ; i < k.length; i++, j--) {
+			k[i] = p[j];
+		}
+		System.out.println(Arrays.toString(k));
 
 		
 		
--- 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_3.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_3.java
@@ -1,5 +1,10 @@
 package step2_01.array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+//2023.03.21 12:29 ~ 12:44
+
 public class ArrayEx24_테스트문제1_3 {
 
 	public static void main(String[] args) {
@@ -10,6 +15,14 @@ public class ArrayEx24_테스트문제1_3 {
 		int[] l = { 10, 20, 30, 40, 50 };
 		int[] m = { 13, 54, 17, 42, 1 };
 		int[] n = { 0,0,0,0,0};
+		
+		for (int i = 0, j = 0; i < n.length; i++) {
+			if ( (l[i] + m[i]) % 2 == 0) {
+				n[j] = l[i] + m[i];
+				j++;
+			}
+		}
+		System.out.println(Arrays.toString(n));
 				
 				
 		// 문제 14) 아래는 시험결과 이다. 시험에 합격한 사람의 번호만 win에 저장 (60점이상 합격)
@@ -18,11 +31,27 @@ public class ArrayEx24_테스트문제1_3 {
 		int[] win = { 0,0,0 };
 		// 예) win[3]= {1002, 1003, 0};
 		
+		for (int i = 0, j = 0; i < win.length; i++) {
+			if ( score[i] >= 60 ) {
+				win[j] = num[i];
+				j++;
+			}
+		}
+		System.out.println(Arrays.toString(win));
 		
 		// 문제 15) 아래는 시험결과이다. 시험에 합격한 사람의 번호만 win2에 저장 (60점이상 합격)
 		int[] data = { 1001, 80 , 1002, 23, 1003 , 78 };
 		int[] win2 = { 0,0,0 };
 		// 예) win2[3] = {1001, 1003, 0};
+		
+		for (int i = 1, j = 0; i < data.length; i++) {
+			if ( data[i] >= 60 ) {
+				win2[j] = data[i-1];
+				j++;
+			}
+			i++;
+		}
+		System.out.println(Arrays.toString(win2));
 		
 	
 		// 문제 16) 아래 배열o 에서 내가 입력한 값만 빼고 p배열에 저장 
@@ -30,12 +59,35 @@ public class ArrayEx24_테스트문제1_3 {
 		int[] p = { 0,0,0,0,0 };
 		// 예) 30 ==> p = {10,20,40,50,0};
 		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("값 입력 >> ");
+		int val = scan.nextInt();
+		
+		for (int i = 0, j = 0; i < p.length; i++) {
+			if (o[i] == val ) continue;
+			p[j] = o[i];
+			j++;
+		}
+		System.out.println(Arrays.toString(p));
 		
 		// 문제 17) 아래 배열 q에서 내가 입력한 번호와 값만 빼고 다른 번호와 값을 r 에 저장 
 		int[] q = { 1001, 40, 1002, 65, 1003,  70 };
 		int[] r = { 0,0,0,0,0,0 };
 		// 예) 1002 ==> r = {1001, 40, 1003, 70 , 0, 0};
 		
+		System.out.print("값 입력 >> ");
+		val = scan.nextInt();
+		
+		for (int i = 0, j = 0; i < r.length; i++) {
+			if (q[i] == val ) {
+				i++;
+				continue;
+			}
+			r[j] = q[i];
+			j++;
+		}
+		
+		System.out.println(Arrays.toString(r));
 		
 		// 문제 18) 
 		int[] arr = {10,20,30,40,50};
@@ -50,6 +102,21 @@ public class ArrayEx24_테스트문제1_3 {
 		
 		// 예)  30, 40, 1, 10, 2
 		// s = { 2, 3, -1, 0, -1}
+		
+		for (int i = 0; i < arr.length; i++) {
+			val = scan.nextInt();
+			int flag = 0;
+			int id;
+			for (int j = 0; j < s.length; j++) {
+				if ( arr[j] == val ) {
+					s[i] = j;
+					flag = 1;
+				}
+			}
+			if ( flag == 0 ) s[i] = -1;
+		}
+		
+		System.out.println(Arrays.toString(s));
 				
 		
 	}
